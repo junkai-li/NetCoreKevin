621418afb0b12848d859650ec3ba9e1aa58c2e56
lijunkai <18570740519@163.com>
2021-06-30 11:27:05 +0800

    数据对比

---
+++ Repository/Attributes/ForeignTableAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository.Attributes
+{
+
+    /// <summary>
+    /// 外连表名称
+    /// </summary>
+    public class ForeignTableAttribute : Attribute
+    {
+
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+
+    }
+}
---
+++ Repository/Attributes/SkipDataComparisonAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Attributes
+{
+    /// <summary>
+    /// 用于跳过数据比对
+    /// </summary>
+    public class SkipDataComparisonAttribute : Attribute
+    {
+
+    }
+}
