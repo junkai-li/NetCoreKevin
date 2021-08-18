2155e1a1260716f5124ce5f31eed63e9f57464ff
lijunkai <18570740519@163.com>
2021-08-18 20:38:29 +0800

    调整项目启动

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -2,29 +2,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Versioning;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Models.JwtBearer;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.IO;
-using System.Linq;
 using System.Text;
-using TencentService;
-using TencentService._;
-using WebApi.Actions;
 using WebApiService.Expand;
-using WebApiService.Filters;
 using WebApiService.Libraries.Swagger;
 
 namespace WebApi
@@ -35,11 +25,7 @@ namespace WebApi
         {
             Configuration = configuration;
         }
-
-
         public IConfiguration Configuration { get; }
-
-
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -48,134 +34,7 @@ namespace WebApi
             //为各数据库注入连接字符串
             Repository.Database.dbContext.ConnectionString = Configuration.GetConnectionString("dbConnection");
             services.AddDbContextPool<Repository.Database.dbContext>(options => { }, 100);
-
-
-            services.Configure<FormOptions>(options =>
-            {
-                options.MultipartBodyLengthLimit = long.MaxValue;
-            });
-
-            services.Configure<KestrelServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
-
-            services.Configure<IISServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
-
-            services.AddHsts(options =>
-            {
-                options.MaxAge = TimeSpan.FromDays(365);
-            });
-
-            services.AddControllers();
-
-
-
-            //注册JWT认证机制
-            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
-            var jwtSettings = new JwtSettings();
-            Configuration.Bind("JwtSettings", jwtSettings);
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(o =>
-            {
-                //主要是jwt  token参数设置
-                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                {
-                    ValidIssuer = jwtSettings.Issuer,
-                    ValidAudience = jwtSettings.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
-                    ValidateLifetime = false
-
-                    /***********************************TokenValidationParameters的参数默认值***********************************/
-                    // RequireSignedTokens = true,
-                    // SaveSigninToken = false,
-                    // ValidateActor = false,
-                    // 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
-                    // ValidateAudience = true,
-                    // ValidateIssuer = true, 
-                    // ValidateIssuerSigningKey = false,
-                    // 是否要求Token的Claims中必须包含Expires
-                    // RequireExpirationTime = true,
-                    // 允许的服务器时间偏移量
-                    // ClockSkew = TimeSpan.FromSeconds(300),
-                    // 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
-                    // ValidateLifetime = true
-
-                };
-            });
-
-
-            //注册HttpContext
-            WebApiService.Libraries.Http.HttpContext.Add(services);
-             
-            //注册全局过滤器
-            services.AddMvc(config => {
-                config.Filters.Add(new GlobalFilter());
-                config.Filters.Add(new PrivilegeFilter());//权限
-            });
-
-            //注册跨域信息
-            services.AddCors(option =>
-            {
-                option.AddPolicy("cors", policy =>
-                {
-                    policy.SetIsOriginAllowed(origin => true)
-                       .AllowAnyHeader()
-                       .AllowAnyMethod()
-                       .AllowCredentials();
-                });
-            });
-
-
-            services.AddControllers().AddJsonOptions(option =>
-            {
-                option.JsonSerializerOptions.Converters.Add(new Common.Json.DateTimeConverter());
-                option.JsonSerializerOptions.Converters.Add(new Common.Json.DateTimeNullConverter());
-                option.JsonSerializerOptions.Converters.Add(new Common.Json.LongConverter());
-            });
-
-
-
-            //注册配置文件信息
-            WebApiService.Libraries.Start.StartConfiguration.Add(Configuration);
-
-
-            services.AddApiVersioning(options =>
-            {
-                //通过Header向客户端通报支持的版本
-                options.ReportApiVersions = true;
-
-                //允许不加版本标记直接调用接口
-                options.AssumeDefaultVersionWhenUnspecified = true;
-
-                //接口默认版本
-                //options.DefaultApiVersion = new ApiVersion(1, 0);
-
-                //如果未加版本标记默认以当前最高版本进行处理
-                options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
-
-                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
-                //options.ApiVersionReader = new QueryStringApiVersionReader("api-version");
-            });
-
-
-            services.AddVersionedApiExplorer(options =>
-            {
-                options.GroupNameFormat = "'v'VVV";
-                options.SubstituteApiVersionInUrl = true;
-            });
-
-
-
-
+            services.ConfigServies(Configuration);
             //注册Swagger生成器，定义一个和多个Swagger 文档
             services.AddSwaggerGen(options =>
             {
@@ -216,38 +75,13 @@ namespace WebApi
                 });
             });
 
-
-
-            //注册统一模型验证
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-
-                    //获取验证失败的模型字段 
-                    var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0).Select(e => e.Value.Errors.First().ErrorMessage).ToList();
-
-                    var dataStr = string.Join(" | ", errors);
-
-                    //设置返回内容
-                    var result = new
-                    {
-                        errMsg = dataStr
-                    };
-
-                    return new BadRequestObjectResult(result);
-                };
-            });
-
             //腾讯MiniLive服务注册
             //MiniLive.AppId = "wxf164719d9baf8d83";
             //MiniLive.AppSecret = "7c635fbff5974b3919826e0cdcf4c8c4";
             //TencentService.Helper.RedisHelper.ConnectionString = Configuration.GetConnectionString("redisConnection");
-            //services.AddSingleton<IMiniLive, MiniLive>(); 
-            //其他
-            services.ConfigServies();
+            //services.AddSingleton<IMiniLive, MiniLive>();  
         }
-         
+
         //向 Startup.Configure 方法添加中间件组件的顺序定义了针对请求调用这些组件的顺序，以及响应的相反顺序。 此顺序对于安全性、性能和功能至关重要。
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
@@ -259,7 +93,6 @@ namespace WebApi
                 context.Request.EnableBuffering();
                 return next(context);
             });
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -269,41 +102,12 @@ namespace WebApi
                 //注册全局异常处理机制
                 app.UseExceptionHandler(builder => builder.Run(async context => await WebApiService.Actions.GlobalError.ErrorEvent(context)));
             }
-
-            app.UseHsts();
-
-
+            //kevin初始化
+            app.UseKevin();
             //注册HttpContext
             WebApiService.Libraries.Http.HttpContext.Initialize(app, env);
-
-
-            //注册跨域信息
-            app.UseCors("cors");
-
-            //强制重定向到Https
-            app.UseHttpsRedirection();
-
-            //静态文件中间件 (UseStaticFiles) 返回静态文件，并简化进一步请求处理。
-            //app.UseStaticFiles();
-
-            app.UseRouting();
-
-            //注册用户认证机制,必须放在 UseCors UseRouting 之后
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
             //注册HostingEnvironment
             WebApiService.Libraries.Start.StartHostingEnvironment.Add(env);
-
-
-            //启用中间件服务生成Swagger作为JSON端点
-            app.UseSwagger();
-
             //启用中间件服务对swagger-ui，指定Swagger JSON端点
             app.UseSwaggerUI(options =>
             {
--- WebApiService/Expand/ServiceConfiguration.cs
+++ WebApiService/Expand/ServiceConfiguration.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Models.JwtBearer;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using WebApiService.Filters;
 using WebApiService.Global;
 using WebApiService.Libraries.Swagger;
 
@@ -12,14 +23,190 @@ namespace WebApiService.Expand
 {
     public static class ServiceConfiguration
     {
-        public static IServiceCollection ConfigServies(this IServiceCollection service)
-        { 
+        public static IServiceCollection ConfigServies(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.Configure<FormOptions>(options =>
+            {
+                options.MultipartBodyLengthLimit = long.MaxValue;
+            });
 
-            service.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigureOptions>();
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            services.AddHsts(options =>
+            {
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
+
+            services.AddControllers();
+
+
+
+            //注册JWT认证机制
+            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind("JwtSettings", jwtSettings);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(o =>
+            {
+                //主要是jwt  token参数设置
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+                    ValidateLifetime = false
+
+                    /***********************************TokenValidationParameters的参数默认值***********************************/
+                    // RequireSignedTokens = true,
+                    // SaveSigninToken = false,
+                    // ValidateActor = false,
+                    // 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
+                    // ValidateAudience = true,
+                    // ValidateIssuer = true, 
+                    // ValidateIssuerSigningKey = false,
+                    // 是否要求Token的Claims中必须包含Expires
+                    // RequireExpirationTime = true,
+                    // 允许的服务器时间偏移量
+                    // ClockSkew = TimeSpan.FromSeconds(300),
+                    // 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
+                    // ValidateLifetime = true
+
+                };
+            });
+
+
+            //注册HttpContext
+            WebApiService.Libraries.Http.HttpContext.Add(services);
+
+            //注册全局过滤器
+            services.AddMvc(config => {
+                config.Filters.Add(new GlobalFilter());
+                config.Filters.Add(new PrivilegeFilter());//权限
+            });
+
+            //注册跨域信息
+            services.AddCors(option =>
+            {
+                option.AddPolicy("cors", policy =>
+                {
+                    policy.SetIsOriginAllowed(origin => true)
+                       .AllowAnyHeader()
+                       .AllowAnyMethod()
+                       .AllowCredentials();
+                });
+            });
+
+
+            services.AddControllers().AddJsonOptions(option =>
+            {
+                option.JsonSerializerOptions.Converters.Add(new Common.Json.DateTimeConverter());
+                option.JsonSerializerOptions.Converters.Add(new Common.Json.DateTimeNullConverter());
+                option.JsonSerializerOptions.Converters.Add(new Common.Json.LongConverter());
+            });
+
+
+
+            //注册配置文件信息
+            WebApiService.Libraries.Start.StartConfiguration.Add(Configuration);
+
+
+            services.AddApiVersioning(options =>
+            {
+                //通过Header向客户端通报支持的版本
+                options.ReportApiVersions = true;
+
+                //允许不加版本标记直接调用接口
+                options.AssumeDefaultVersionWhenUnspecified = true;
+
+                //接口默认版本
+                //options.DefaultApiVersion = new ApiVersion(1, 0);
+
+                //如果未加版本标记默认以当前最高版本进行处理
+                options.ApiVersionSelector = new CurrentImplementationApiVersionSelector(options);
+
+                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                //options.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+            });
+
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            }); 
+
+            //注册统一模型验证
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+
+                    //获取验证失败的模型字段 
+                    var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0).Select(e => e.Value.Errors.First().ErrorMessage).ToList();
+
+                    var dataStr = string.Join(" | ", errors);
+
+                    //设置返回内容
+                    var result = new
+                    {
+                        errMsg = dataStr
+                    };
+
+                    return new BadRequestObjectResult(result);
+                };
+            });
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigureOptions>();
             //注册雪花ID算法示例
-            service.AddSingleton(new Common.SnowflakeHelper(0, 0));
-            GlobalServices.ServiceProvider=service.BuildServiceProvider();
-            return service;
+            services.AddSingleton(new Common.SnowflakeHelper(0, 0));
+            GlobalServices.ServiceProvider= services.BuildServiceProvider();
+            return services;
+        }
+
+        /// <summary>
+        /// 使用UseKevin
+        /// UseKevin
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseKevin(this IApplicationBuilder app)
+        { 
+            app.UseHsts(); 
+            //注册跨域信息
+            app.UseCors("cors");
+            //强制重定向到Https
+            app.UseHttpsRedirection();
+
+            //静态文件中间件 (UseStaticFiles) 返回静态文件，并简化进一步请求处理。
+            //app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //注册用户认证机制,必须放在 UseCors UseRouting 之后
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            //启用中间件服务生成Swagger作为JSON端点
+            app.UseSwagger();
+
+            return app;
         }
     }
 }
