fc4eb2e9f893301c134296c745fbf3f78ee12438
lijunkai <18570740519@163.com>
2021-10-29 15:05:09 +0800

    随机生成英文用户名 中文用户名

--- Common/StringHelper.cs
+++ Common/StringHelper.cs
@@ -34,7 +34,33 @@ namespace Common
             return orderno;
         }
 
-
+        /// <summary>
+        /// 生成用户名
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string GetRandomString(int length)
+        {
+            string randStr = "";
+            Random rd = new Random();
+            byte[] str = new byte[length];
+            int i;
+            for (i = 0; i < length - 1; i++)
+            {
+                int a = 0;
+                while (!((a >= 48 && a <= 57) || (a >= 97 && a <= 122)))
+                {
+                    a = rd.Next(48, 122);
+                }
+                str[i] = (byte)a;
+            }
+            string username = new string(UnicodeEncoding.ASCII.GetChars(str));
+            Random r = new Random(unchecked((int)DateTime.Now.Ticks));
+            string s1 = ((char)r.Next(97, 122)).ToString();
+            username = username.Replace("/0", "");
+            randStr = s1 + username;
+            return randStr;
+        }
 
         /// <summary>
         /// 移除字符串中的全部标点符号
@@ -418,5 +444,88 @@ namespace Common
         }
 
         #endregion
+
+        /// <summary>
+        /// 生成中文用户名
+        /// </summary>
+        /// <param name="strlength"></param>
+        /// <param name="isRandomCount"></param>
+        /// <returns></returns>
+        public static string GetRandomStringZh(int strlength, bool isRandomCount = false)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            //获取GB2312编码页（表）
+            Encoding gb = Encoding.GetEncoding("gb2312");
+            string name = string.Empty;
+
+            if (isRandomCount)
+            {
+                Random random = new Random();
+                strlength = random.Next(1, strlength + 1);
+            }
+
+            //定义一个字符串数组储存汉字编码的组成元素
+            string[] rBase = new String[16] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+            Random rnd = new Random();
+            //定义一个object数组用来
+            object[] bytes = new object[strlength];
+            /*每循环一次产生一个含两个元素的十六进制字节数组，并将其放入bject数组中
+             每个汉字有四个区位码组成
+             区位码第1位和区位码第2位作为字节数组第一个元素
+             区位码第3位和区位码第4位作为字节数组第二个元素
+            */
+
+            for (int i = 0; i < strlength; i++)
+            {
+                //区位码第1位
+                int r1 = rnd.Next(11, 14);
+                string str_r1 = rBase[r1].Trim();
+                //区位码第2位
+                rnd = new Random(r1 * unchecked((int)DateTime.Now.Ticks) + i);//更换随机数发生器的种子避免产生重复值
+                int r2;
+                if (r1 == 13)
+                {
+                    r2 = rnd.Next(0, 7);
+                }
+                else
+                {
+                    r2 = rnd.Next(0, 16);
+                }
+                string str_r2 = rBase[r2].Trim();
+                //区位码第3位
+                rnd = new Random(r2 * unchecked((int)DateTime.Now.Ticks) + i);
+                int r3 = rnd.Next(10, 16);
+                string str_r3 = rBase[r3].Trim();
+                //区位码第4位
+                rnd = new Random(r3 * unchecked((int)DateTime.Now.Ticks) + i);
+                int r4;
+                if (r3 == 10)
+                {
+                    r4 = rnd.Next(1, 16);
+                }
+                else if (r3 == 15)
+                {
+                    r4 = rnd.Next(0, 15);
+                }
+                else
+                {
+                    r4 = rnd.Next(0, 16);
+                }
+                string str_r4 = rBase[r4].Trim();
+                //定义两个字节变量存储产生的随机汉字区位码
+                byte byte1 = Convert.ToByte(str_r1 + str_r2, 16);
+                byte byte2 = Convert.ToByte(str_r3 + str_r4, 16);
+                //将两个字节变量存储在字节数组中
+                byte[] str_r = new byte[] { byte1, byte2 };
+                //将产生的一个汉字的字节数组放入object数组中
+                bytes.SetValue(str_r, i);
+            }
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                name += gb.GetString((byte[])Convert.ChangeType(bytes[i], typeof(byte[])));
+            }
+            return name;
+        }
     }
 }
