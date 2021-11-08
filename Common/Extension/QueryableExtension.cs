b10a8c520ac3011f07e340b47251e6f019e54195
lijunkai <18570740519@163.com>
2021-11-08 15:37:59 +0800

    根据指定属性名称对序列进行排序

--- Common/Extension/QueryableExtension.cs
+++ Common/Extension/QueryableExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace System.Linq
 {
@@ -81,6 +82,25 @@ namespace System.Linq
 
             IQueryable<TSource> q = source.Skip(skipCount).Take(takeCount);
             return q;
+        }
+
+        /// <summary>  
+        /// 根据指定属性名称对序列进行排序  
+        /// </summary>  
+        /// <typeparam name="TSource">source中的元素的类型</typeparam>  
+        /// <param name="source">一个要排序的值序列</param>  
+        /// <param name="property">属性名称</param>  
+        /// <param name="descending">是否降序</param>  
+        /// <returns></returns>  
+        public static IQueryable<TSource> OrderBy<TSource>(this IQueryable<TSource> source, string property, bool descending) where TSource : class
+        {
+            ParameterExpression param = Expression.Parameter(typeof(TSource), "c");
+            PropertyInfo pi = typeof(TSource).GetProperty(property);
+            MemberExpression selector = Expression.MakeMemberAccess(param, pi);
+            LambdaExpression le = Expression.Lambda(selector, param);
+            string methodName = (descending) ? "OrderByDescending" : "OrderBy";
+            MethodCallExpression resultExp = Expression.Call(typeof(Queryable), methodName, new Type[] { typeof(TSource), pi.PropertyType }, source.Expression, le);
+            return source.Provider.CreateQuery<TSource>(resultExp);
         }
     }
 }
