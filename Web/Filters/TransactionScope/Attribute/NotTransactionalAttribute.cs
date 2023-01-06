a2b1699e525755c58e7362d1ca98fed51fef8b85
lijunkai <18570740519@163.com>
2023-01-06 14:12:37 +0800

    自动启用事务的筛选器

---
+++ Web/Filters/TransactionScope/Attribute/NotTransactionalAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kevin.Web.Filters.TransactionScope.Attribute
+{
+    /// <summary>
+    /// 不启动
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class NotTransactionalAttribute : System.Attribute
+    {
+    }
+}
---
+++ Web/Filters/TransactionScope/TransactionScopeFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Transactions;
+using Kevin.Web.Filters.TransactionScope.Attribute;
+
+namespace Kevin.Web.Filters.TransactionScope
+{
+    /// <summary>
+    /// 自动启用事务的筛选器
+    /// </summary>
+    public class TransactionScopeFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            bool hasNotTransactionalAttribute = false;
+            if (context.ActionDescriptor is ControllerActionDescriptor)
+            {
+                var actionDesc = (ControllerActionDescriptor)context.ActionDescriptor;
+                hasNotTransactionalAttribute = actionDesc.MethodInfo
+                    .IsDefined(typeof(NotTransactionalAttribute));
+            }
+            if (hasNotTransactionalAttribute)
+            {
+                await next();
+                return;
+            }
+            using var txScope =
+                new System.Transactions.TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+            var result = await next();
+            if (result.Exception == null)
+            {
+                txScope.Complete();
+            }
+        }
+    }
+}
