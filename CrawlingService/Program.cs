916d0e6dfd46b26302203ee4dcaec6021600f562
lijunkai <18570740519@163.com>
2023-02-27 11:36:37 +0800

    基于Selenium.WebDriver.MSEdgeDriver用于爬虫数据||自动化测试

---
+++ CrawlingService/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CrawlingService.Task;
+
+Console.WriteLine("Hello, World!");
+Mian.Run();
---
+++ CrawlingService/Task/Mian.cs
@@ -0,0 +1,17 @@
+﻿using CrawlingService.Task.Pitchhub36krCom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlingService.Task
+{
+    public class Mian
+    {
+        public static void Run()
+        {
+            Pitchhub36krComMian.Run();
+        }
+    }
+}
