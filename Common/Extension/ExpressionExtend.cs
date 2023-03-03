630ecef85de524abc26a1759dc71ce779a38032e
lijunkai <18570740519@163.com>
2023-03-03 16:54:33 +0800

    并表达式 And Or  Not扩展

---
+++ Common/Extension/ExpressionExtend.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Kevin.Common.Extension
+{
+    /// <summary>
+    /// 合并表达式 And Or  Not扩展
+    /// </summary>
+    public static class ExpressionExtend
+
+    {
+        public static Expression<Func<T, bool>> And<T>(
+          this Expression<Func<T, bool>> first,
+          Expression<Func<T, bool>> second)
+        {
+            return first.AndAlso<T>(second, Expression.AndAlso);
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(
+            this Expression<Func<T, bool>> first,
+            Expression<Func<T, bool>> second)
+        {
+            return first.AndAlso<T>(second, Expression.OrElse);
+        }
+
+        private static Expression<Func<T, bool>> AndAlso<T>(
+        this Expression<Func<T, bool>> expr1,
+        Expression<Func<T, bool>> expr2,
+        Func<Expression, Expression, BinaryExpression> func)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expr1.Body);
+
+            var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expr2.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                func(left, right), parameter);
+        }
+
+        private class ReplaceExpressionVisitor
+            : ExpressionVisitor
+        {
+            private readonly Expression _oldValue;
+            private readonly Expression _newValue;
+
+            public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+            {
+                _oldValue = oldValue;
+                _newValue = newValue;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                if (node == _oldValue)
+                    return _newValue;
+                return base.Visit(node);
+            }
+        }
+    }
+}
