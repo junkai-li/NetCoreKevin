e110e4c15cb33c073aa2af8a6bd8be49a8a02692
lijunkai <18570740519@163.com>
2023-05-08 11:13:25 +0800

    ReplaceIocControllerActivator

--- Web/Extensions/ServiceConfiguration.cs
+++ Web/Extensions/ServiceConfiguration.cs
@@ -155,7 +155,7 @@ namespace Web.Extension
 
             //把控制器作为服务注册，然后使用它内置的ioc来替换原来的控制器的创建器
             services.AddControllersWithViews().AddControllersAsServices();
-            services.Replace(ServiceDescriptor.Transient<IControllerActivator, IocServiceBaseControllerActivator>());
+            services.ReplaceIocControllerActivator();
             //App服务注册
             RegisterAppServices(services, Configuration);
             services.AddKevinPermission();
