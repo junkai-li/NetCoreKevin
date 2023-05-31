fbcd8c295caceb71b8191585f8abf537939b3167
lijunkai <18570740519@163.com>
2023-05-31 16:29:58 +0800

    单元测试

--- Kevin.Test/UnitTest1.cs
+++ Kevin.Test/UnitTest1.cs
@@ -1,11 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
 namespace Kevin.Test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataRow(" 1==1 ")]
+        public void TsetDynamicExpresso(string str)
         {
+            var interpreter = new DynamicExpresso.Interpreter();
+            Assert.IsFalse(!interpreter.Eval<bool>(str), $"{str} TsetDynamicExpresso");
         }
+
     }
 }
