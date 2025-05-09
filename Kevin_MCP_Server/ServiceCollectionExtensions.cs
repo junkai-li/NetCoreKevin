4f77053832dcc5917bb1494fbfedda7a6aa64fb4
lijunkai <18570740519@163.com>
2025-05-09 11:28:20 +0800

    删除mcp服务端得OpenTelemetry服务

--- Kevin_MCP_Server/ServiceCollectionExtensions.cs
+++ Kevin_MCP_Server/ServiceCollectionExtensions.cs
@@ -1,10 +1,7 @@
-﻿using Google.Protobuf.WellKnownTypes;
+﻿ 
 using Kevin_MCP_Server.Tools;
 using Microsoft.Extensions.DependencyInjection;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Trace;
-
+ 
 namespace Kevin_MCP_Server
 {
     public static class ServiceCollectionExtensions
@@ -25,15 +22,15 @@ namespace Kevin_MCP_Server
             //WithMetrics()：配置度量，包括所有计量器（AddMeter("*")），ASP.NET Core和HTTP客户端的自动仪器化。
             //WithLogging()：启用日志记录。
             //UseOtlpExporter()：使用OTLP（OpenTelemetry Protocol）导出器将遥测数据发送
-            services.AddOpenTelemetry()
-                        .WithTracing(b => b.AddSource("*")
-                            .AddAspNetCoreInstrumentation()
-                            .AddHttpClientInstrumentation())
-                        .WithMetrics(b => b.AddMeter("*")
-                            .AddAspNetCoreInstrumentation()
-                            .AddHttpClientInstrumentation())
-                        .WithLogging()
-                        .UseOtlpExporter();
+            //services.AddOpenTelemetry()
+            //            .WithTracing(b => b.AddSource("*")
+            //                .AddAspNetCoreInstrumentation()
+            //                .AddHttpClientInstrumentation())
+            //            .WithMetrics(b => b.AddMeter("*")
+            //                .AddAspNetCoreInstrumentation()
+            //                .AddHttpClientInstrumentation())
+            //            .WithLogging()
+            //            .UseOtlpExporter();
         }
     }
 }
