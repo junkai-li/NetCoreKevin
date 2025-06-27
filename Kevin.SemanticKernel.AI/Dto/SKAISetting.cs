ef94f67c879afe72c6b097ab7521a72d065345ae
lijunkai <18570740519@163.com>
2025-06-27 17:16:17 +0800

    Kevin.AI.SemanticKernel

---
+++ Kevin.SemanticKernel.AI/Dto/SKAISetting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kevin.AI.SemanticKernel.Dto
+{
+    public class SKAISetting
+    {
+        /// <summary>
+        /// 模型名称
+        /// </summary>
+        public string modelId { get; set; } = "glm-4-flash";
+
+        /// <summary>
+        /// 默认使用智谱AI的模型，如果需要使用自己的模型，请修改endpoint和apiKey
+        /// </summary>
+        public string endpoint { get; set; } = "https://open.bigmodel.cn/api/paas/v4/";
+
+            /// <summary>
+            ///根据自己的APIKEY进行修改
+            /// </summary>
+        public string apiKey { get; set; } = "******";
+    }
+}
---
+++ Kevin.SemanticKernel.AI/KevinAIClient.cs
@@ -0,0 +1,35 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kevin.AI.SemanticKernel
+{
+    public class KevinAIClient : IKevinAIClient
+    {
+        public ChatHistory history = new ChatHistory();
+        private readonly Kernel _kernel;
+        public KevinAIClient(Kernel kernel)
+        {
+            _kernel = kernel;
+            history.AddSystemMessage("你是NetCoreKevin框架的通用聊天助手");
+        }
+
+        public string SendMsg(string msg)
+        {
+            history.AddUserMessage(msg);
+            var arguments = new KernelArguments { { "userMessage", msg } };
+            // 获取聊天函数
+            var chatFunction = _kernel.Plugins.GetFunction("kevinai", "msg"); ;
+            FunctionResult result = chatFunction.InvokeAsync(_kernel, arguments).Result;
+            var data = result.ToString();
+            history.AddAssistantMessage(data);
+            return data;
+        }
+    }
+}
