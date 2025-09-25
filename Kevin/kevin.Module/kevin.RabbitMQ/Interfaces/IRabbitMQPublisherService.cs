17d83dfe15d5de94fc0e62d52d74ca076738c92a
lijunkai <18570740519@163.com>
2025-09-25 16:39:54 +0800

    死信交换机

--- Kevin/kevin.Module/kevin.RabbitMQ/Interfaces/IRabbitMQPublisherService.cs
+++ Kevin/kevin.Module/kevin.RabbitMQ/Interfaces/IRabbitMQPublisherService.cs
@@ -20,11 +20,16 @@ namespace kevin.RabbitMQ.Interfaces
         /// <param name="exchangeType">交换机类型模式 ExchangeType</param>
         /// <param name="isTx">是否开启事务</param>
         /// <param name="isConfirmSelect">是否确认机制</param>
-        /// <param name="BasicAcks">确认机制成功回调</param>
-        /// <param name="BasicNacks">确认机制失败回调</param>
+        /// <param name="basicAcks">确认机制成功回调</param>
+        /// <param name="basicNacks">确认机制失败回调</param>
+        /// <param name="isTtl">是否开启TTL机制</param>
+        /// <param name="ttl">TTL时间</param>
+        /// <param name="dlxExchange">死信交换机</param>
+        /// <param name="dlxRoutingKey">死信路由键</param>
         /// <exception cref="UserFriendlyException">确认机制开启，必须传入BasicAcks和BasicNacks</exception>
-        void PublishList<T>(string exchange, string routingKey,List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? BasicAcks = default, EventHandler<BasicNackEventArgs>? BasicNacks = default) where T : class;
-        Task PublishListAsync<T>(string exchange, string routingKey, List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? BasicAcks = default, EventHandler<BasicNackEventArgs>? BasicNacks = default) where T : class;
+        /// <exception cref="UserFriendlyException">开启TTL机制消息过期必须传入死信交换机和死信路由</exception>
+        void PublishList<T>(string exchange, string routingKey,List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? basicAcks = default, EventHandler<BasicNackEventArgs>? basicNacks = default, bool isTtl = false, int ttl = 30000, string? dlxExchange = "", string? dlxRoutingKey = "") where T : class;
+        Task PublishListAsync<T>(string exchange, string routingKey, List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? basicAcks = default, EventHandler<BasicNackEventArgs>? basicNacks = default, bool isTtl = false, int ttl = 30000, string? dlxExchange = "", string? dlxRoutingKey = "") where T : class;
  
     }
 }
--- Kevin/kevin.Module/kevin.RabbitMQ/RabbitMQPublisherService.cs
+++ Kevin/kevin.Module/kevin.RabbitMQ/RabbitMQPublisherService.cs
@@ -3,14 +3,14 @@ using Kevin.log4Net;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
-using System.Text.Json; 
+using System.Text.Json;
 using Web.Global.Exceptions;
 namespace kevin.RabbitMQ
 {
     public class RabbitMQPublisherService : IRabbitMQPublisherService, IDisposable
     {
         private readonly IRabbitMQConnection _connection;
-        private readonly IModel _channel;  
+        private readonly IModel _channel;
         private bool _isDisposed;
         public RabbitMQPublisherService(IRabbitMQConnection connection)
         {
@@ -18,6 +18,7 @@ namespace kevin.RabbitMQ
             //创建通道
             _channel = _connection.CreateModel();
         }
+
         /// <summary>
         /// 发送消息
         /// </summary>
@@ -29,31 +30,53 @@ namespace kevin.RabbitMQ
         /// <param name="exchangeType">交换机类型模式 ExchangeType</param>
         /// <param name="isTx">是否开启事务</param>
         /// <param name="isConfirmSelect">是否确认机制</param>
-        /// <param name="BasicAcks">确认机制成功回调</param>
-        /// <param name="BasicNacks">确认机制失败回调</param>
+        /// <param name="basicAcks">确认机制成功回调</param>
+        /// <param name="basicNacks">确认机制失败回调</param>
+        /// <param name="isTtl">是否开启TTL机制</param>
+        /// <param name="ttl">TTL时间</param>
+        /// <param name="dlxExchange">死信交换机</param>
+        /// <param name="dlxRoutingKey">死信路由键</param>
         /// <exception cref="UserFriendlyException">确认机制开启，必须传入BasicAcks和BasicNacks</exception>
-        public void PublishList<T>(string exchange, string routingKey,List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? BasicAcks = default, EventHandler<BasicNackEventArgs>? BasicNacks = default) where T : class
+        /// <exception cref="UserFriendlyException">开启TTL机制消息过期必须传入死信交换机和死信路由</exception>
+        public void PublishList<T>(string exchange, string routingKey, List<T> messages,
+                                string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false,
+                                EventHandler<BasicAckEventArgs>? basicAcks = default, EventHandler<BasicNackEventArgs>? basicNacks = default,
+                                bool isTtl = false, int ttl = 30000, string? dlxExchange = "", string? dlxRoutingKey = ""
+            ) where T : class
         {
+            var args = new Dictionary<string, object>();
 
             #region 校验
-          
+
             if (isConfirmSelect)
             {
-                if (BasicAcks == default || BasicNacks == default)
+                if (basicAcks == default || basicNacks == default)
                 {
                     throw new UserFriendlyException("确认机制开启，必须传入BasicAcks和BasicNacks");
                 }
             }
+            if (isTtl)
+            {
+                if (string.IsNullOrEmpty(dlxExchange) || string.IsNullOrEmpty(dlxRoutingKey))
+                {
+                    throw new UserFriendlyException("开启TTL机制消息过期必须传入死信交换机和死信路由");
+                }
+                args = new Dictionary<string, object>
+                {
+                    { "x-dead-letter-exchange", dlxExchange }, //死信交换机});
+                    { "x-dead-letter-routing-key", dlxRoutingKey } //死信路由键
+                };
+            }
 
             #endregion
-          
+
 
             try
             {
                 //声明队列
                 if (!string.IsNullOrEmpty(queue))
                 {
-                    _channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false);
+                    _channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false, args);
                     _channel.QueueBind(queue, exchange, routingKey);
                 }
 
@@ -69,22 +92,25 @@ namespace kevin.RabbitMQ
                 var properties = _channel.CreateBasicProperties();
                 properties.Persistent = true;
                 properties.ContentType = "application/json";
-         
+                if (isTtl)
+                {
+                    properties.Expiration = ttl.ToString();
+                }
                 //开启确认机制
                 if (isConfirmSelect)
                 {
                     _channel.ConfirmSelect();
-                    _channel.BasicAcks += BasicAcks;
-                    _channel.BasicNacks += BasicNacks;
+                    _channel.BasicAcks += basicAcks;
+                    _channel.BasicNacks += basicNacks;
                 }
                 //开启事务
                 if (isTx) { _channel.TxSelect(); }
-              
+
                 foreach (var message in messages)
                 {
                     var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
                     _channel.BasicPublish(exchange, routingKey, false, properties, body);
-                }  
+                }
 
                 //提交事务
                 if (isTx) { _channel.TxCommit(); }
@@ -92,17 +118,35 @@ namespace kevin.RabbitMQ
             }
             catch (Exception ex)
             {
-                LogHelper<RabbitMQPublisherService>.logger.Error($"消息发布失败:An error occurred: {ex.Message}"); 
+                LogHelper<RabbitMQPublisherService>.logger.Error($"消息发布失败:An error occurred: {ex.Message}");
                 //回滚事务
                 if (isTx) { _channel.TxRollback(); }
-                throw; 
+                throw;
             }
-        
-        }
 
-        public Task PublishListAsync<T>(string exchange, string routingKey, List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? BasicAcks = null, EventHandler<BasicNackEventArgs>? BasicNacks = null) where T : class
+        }
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="exchange">交换机</param>
+        /// <param name="routingKey">路由</param>
+        /// <param name="messages">消息</param>
+        /// <param name="queue">队列</param>
+        /// <param name="exchangeType">交换机类型模式 ExchangeType</param>
+        /// <param name="isTx">是否开启事务</param>
+        /// <param name="isConfirmSelect">是否确认机制</param>
+        /// <param name="basicAcks">确认机制成功回调</param>
+        /// <param name="basicNacks">确认机制失败回调</param>
+        /// <param name="isTtl">是否开启TTL机制</param>
+        /// <param name="ttl">TTL时间</param>
+        /// <param name="dlxExchange">死信交换机</param>
+        /// <param name="dlxRoutingKey">死信路由键</param>
+        /// <exception cref="UserFriendlyException">确认机制开启，必须传入BasicAcks和BasicNacks</exception>
+        /// <exception cref="UserFriendlyException">开启TTL机制消息过期必须传入死信交换机和死信路由</exception>
+        public Task PublishListAsync<T>(string exchange, string routingKey, List<T> messages, string queue = "", string exchangeType = "", bool isTx = false, bool isConfirmSelect = false, EventHandler<BasicAckEventArgs>? basicAcks = null, EventHandler<BasicNackEventArgs>? basicNacks = null, bool isTtl = false, int ttl = 30000, string? dlxExchange = "", string? dlxRoutingKey = "") where T : class
         {
-           return Task.Run(() => PublishList(exchange, routingKey, messages, queue, exchangeType, isTx, isConfirmSelect, BasicAcks, BasicNacks));
+            return Task.Run(() => PublishList(exchange, routingKey, messages, queue, exchangeType, isTx, isConfirmSelect, basicAcks, basicNacks, isTtl, ttl, dlxExchange, dlxRoutingKey));
         }
         public void Dispose()
         {
@@ -115,13 +159,13 @@ namespace kevin.RabbitMQ
             if (!_isDisposed)
             {
                 if (disposing)
-                { 
+                {
                     _channel?.Close();
                     _channel?.Dispose();
                     _connection?.Dispose();
                 }
                 _isDisposed = true;
             }
-        } 
+        }
     }
 }
