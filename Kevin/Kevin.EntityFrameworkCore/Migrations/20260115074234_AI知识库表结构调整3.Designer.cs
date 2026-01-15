30a7a472538ca02ac01886397bf3ff41eeedcca6
lijunkai <18570740519@163.com>
2026-01-15 15:57:52 +0800

    表结构调整

--- Kevin/Kevin.EntityFrameworkCore/Migrations/20260115074234_AI知识库表结构调整3.Designer.cs
+++ Kevin/Kevin.EntityFrameworkCore/Migrations/20260115074234_AI知识库表结构调整3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository.Database;
 
@@ -11,9 +12,11 @@ using Repository.Database;
 namespace Kevin.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(KevinDbContext))]
-    partial class KevinDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260115074234_AI知识库表结构调整3")]
+    partial class AI知识库表结构调整3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Kevin/Kevin.EntityFrameworkCore/Migrations/20260115074234_AI知识库表结构调整3.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kevin.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AI知识库表结构调整3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "kms_id_list",
+                table: "t_a_i_apps");
+
+            migrationBuilder.AddColumn<string>(
+                name: "content_name",
+                table: "t_a_i_kms_details",
+                type: "longtext",
+                nullable: false,
+                comment: "content_name")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<long>(
+                name: "kms_id",
+                table: "t_a_i_apps",
+                type: "bigint",
+                nullable: true,
+                comment: "知识库ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_a_i_apps_kms_id",
+                table: "t_a_i_apps",
+                column: "kms_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_t_a_i_apps_t_a_i_kmss_kms_id",
+                table: "t_a_i_apps",
+                column: "kms_id",
+                principalTable: "t_a_i_kmss",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_t_a_i_apps_t_a_i_kmss_kms_id",
+                table: "t_a_i_apps");
+
+            migrationBuilder.DropIndex(
+                name: "IX_t_a_i_apps_kms_id",
+                table: "t_a_i_apps");
+
+            migrationBuilder.DropColumn(
+                name: "content_name",
+                table: "t_a_i_kms_details");
+
+            migrationBuilder.DropColumn(
+                name: "kms_id",
+                table: "t_a_i_apps");
+
+            migrationBuilder.AddColumn<string>(
+                name: "kms_id_list",
+                table: "t_a_i_apps",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                comment: "知识库ID列表")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
