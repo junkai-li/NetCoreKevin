0808d24a23c0d2d6cf99ca3599883e286f84eb23
lijunkai <18570740519@163.com>
2026-01-21 16:39:37 +0800

    reg删除cw输出

--- Kevin/kevin.Module/Kevin.RAG/RAGService.cs
+++ Kevin/kevin.Module/Kevin.RAG/RAGService.cs
@@ -18,9 +18,7 @@ namespace Kevin.RAG
             QdrantClientService = qdrantClientService;
         }
         public async Task<(bool, string, List<DocumentChunkDto>)> GetSystemPrompt(string collectionName, Embedding<float> question, int topK = 3, double? Score = null)
-        {
-            Console.WriteLine($"\n问题：{question}");
-            Console.WriteLine("正在检索相关文档...");
+        { 
             var documents = await QdrantClientService.Search(collectionName, question, (ulong)topK, Score);
             // 4. 构建提示词
             var systemPrompt = @" 
@@ -32,14 +30,14 @@ namespace Kevin.RAG
                                 5. 可以引用文档来源 引用时必须选择相似度最高的文档";
             if (documents.Count == 0)
             {
+               
                 return (false, systemPrompt+"\n" +"抱歉，我没有找到相关的文档来回答您的问题。",new List<DocumentChunkDto>());
             }
-            Console.WriteLine($"找到 {documents.Count} 个相关文档");
+ 
             // 3. 构建上下文
             var context = string.Join("\n\n---\n\n", documents.Select((doc, index) =>
                 $"文档 {doc.Title +"-"+ index + 1}（来源：{doc.SourceFile}）（相似度：{doc.Score}）：\n{doc.Content}"));
-            Console.WriteLine(context);
-
+              
             var userPrompt = $@"文档内容：
                                 {context} 
                                 用户问题：
