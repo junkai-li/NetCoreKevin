8bff25fb1db26785694227a068d28e477262370c
lijunkai <18570740519@163.com>
2026-04-23 10:04:09 +0800

    占位类，确保 Kevin.ALL.Module 项目产出程序集以供引用。

---
+++ Kevin/kevin.Module/Kevin.ALL.Module/KevinAllModuleMarker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kevin.ALL.Module
+{
+    /// <summary>
+    /// 占位类，确保 Kevin.ALL.Module 项目产出程序集以供引用。
+    /// </summary>
+    public static class KevinAllModuleMarker
+    {
+    }
+}
