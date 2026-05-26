924d0356c191cf3c37985138ab244f82c9c9232b
lijunkai <18570740519@163.com>
2026-05-26 11:15:08 +0800

    Kevin.Common

--- Kevin/kevin.Module/Kevin.Common/Helper/EncryptHelper/AESEncryptor.cs
+++ Kevin/kevin.Module/Kevin.Common/Helper/EncryptHelper/AESEncryptor.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace NetCore.Util.EncryptHelper
+namespace Kevin.Common
 {
     public class AESEncryptor
     {
--- Kevin/kevin.Module/Kevin.Common/Helper/EncryptHelper/MD5Encrypt.cs
+++ Kevin/kevin.Module/Kevin.Common/Helper/EncryptHelper/MD5Encrypt.cs
@@ -2,7 +2,7 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace NetCore.Util.EncryptHelper
+namespace Kevin.Common
 {
     /// <summary>
     /// 不可逆加密
