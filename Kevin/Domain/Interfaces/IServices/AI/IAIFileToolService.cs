b7826759793893392c505bc59119dddbb55a5081
lijunkai <18570740519@163.com>
2026-08-20 09:45:42 +0800

    SaveFileContent 必填调整

--- Kevin/Domain/Interfaces/IServices/AI/IAIFileToolService.cs
+++ Kevin/Domain/Interfaces/IServices/AI/IAIFileToolService.cs
@@ -9,6 +9,6 @@ namespace kevin.Domain.Interfaces.IServices.AI
     public interface IAIFileToolService
     {
         [Description("保存文件到远程文件服务器并返回访问url。 本地文件地址和文件内容二选一（必须有一个必填），用于把各种文件输出到远程文件服务器，返回完整url路径或以 ❌ 开头的错误信息。")]
-        string SaveFileContent([Description("文件名称如（xx.html,xx.txt）支持各种文件类型")][Required] string fileName, [Description("本地文件地址")] string basePath, [Description("文件数据内容")] string content);
+        string SaveFileContent([Description("文件名称如（xx.html,xx.txt）支持各种文件类型")][Required] string fileName, [Description("本地文件地址")] string? basePath, [Description("文件数据内容")] string? content);
     }
 }
